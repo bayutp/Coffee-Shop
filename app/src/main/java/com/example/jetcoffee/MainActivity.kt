8a9f6527b52118c1dd899f0fae16383bcaa1c2be
bayupamuji <bayuteguhpamuji@gmail.com>
2023-02-23 11:24:20 +0700

    create banner

---
+++ app/src/main/java/com/example/jetcoffee/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.example.jetcoffee
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.jetcoffee.ui.components.SearchBar
+import com.example.jetcoffee.ui.theme.JetCoffeeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetCoffeeTheme {
+                JetCoffeeApp()
+            }
+        }
+    }
+}
+
+@Composable
+fun JetCoffeeApp() {
+    Column {
+        Banner()
+    }
+}
+
+@Composable
+fun Banner(modifier: Modifier = Modifier) {
+    Box(modifier = modifier) {
+        Image(
+            painter = painterResource(id = R.drawable.banner),
+            contentDescription = "Banner Image",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.height(200.dp)
+        )
+        SearchBar()
+    }
+}
+
+@Preview(
+    name = "Home",
+    showBackground = true,
+    device = Devices.PIXEL_4,
+    showSystemUi = true
+)
+@Composable
+fun DefaultPreview() {
+    JetCoffeeTheme {
+        JetCoffeeApp()
+    }
+}
